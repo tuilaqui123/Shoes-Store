7c774e88741c8be3da9ad83ec477fd1bf56e8142
tuilaqui123 <quidhtv0149@gmail.com>
2023-10-03 20:48:35 +0700

    80

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 Home();
 
-checkOut();
-
-
 function shoppingCart() {
     $(function () {
         $("#display").load("./shopping-cart/cart.html");
     });
 }
 
+function Fav() {
+    $(function () {
+        $("#display").load("./home-page/fav.html");
+    });
+}
+
+function BrandItem() {
+    $(function () {
+        $("#display").load("./home-page/brandItem.html");
+    });
+}
+
 function checkOut() {
     $(function () {
         $("#display").load("./shopping-cart/checkout.html");
+    });
+}
+
+function Shipping() {
+    $(function () {
+        $("#display").load("./shopping-cart/shipping.html");
+    });
+}
+
+function Shoe() {
+    $(function () {
+        $("#display").load("./shoe/shoe.html");
+    });
+}
+
+function orderDetail() {
+    $(function () {
+        $("#display").load("./shopping-cart/orderDetail.html");
     });
 }
 
@@ -74,3 +101,15 @@ function cancelNoti() {
         $("#display").load("./login-register/signIn.html");
     });
 }
+
+//-----------------------
+
+$(document).ready(function () {
+    // Click event to load HTML content into the main div
+
+    $("#display").load("your-content.html", function () {
+        // Once content is loaded, scroll to the top
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+
+});
