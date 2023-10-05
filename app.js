60f00c3cbf7064dbcce15d68d4c91b03cfc1cbdb
tuilaqui123 <quidhtv0149@gmail.com>
2023-10-05 09:21:25 +0700

    90%

--- app.js
+++ app.js
@@ -1,67 +1,103 @@
 Home();
 
+Fav();
+
 function shoppingCart() {
     $(function () {
         $("#display").load("./shopping-cart/cart.html");
+        $('html, body').animate({
+            scrollTop: $("#nav-head").offset().top
+        }, 0);
     });
 }
 
 function Fav() {
     $(function () {
         $("#display").load("./home-page/fav.html");
+        $('html, body').animate({
+            scrollTop: $("#nav-head").offset().top
+        }, 0);
     });
 }
 
 function BrandItem() {
     $(function () {
         $("#display").load("./home-page/brandItem.html");
+        $('html, body').animate({
+            scrollTop: $("#nav-head").offset().top
+        }, 0);
     });
 }
 
 function checkOut() {
     $(function () {
         $("#display").load("./shopping-cart/checkout.html");
+        $('html, body').animate({
+            scrollTop: $("#nav-head").offset().top
+        }, 0);
     });
 }
 
 function Shipping() {
     $(function () {
         $("#display").load("./shopping-cart/shipping.html");
+        $('html, body').animate({
+            scrollTop: $("#nav-head").offset().top
+        }, 0);
     });
 }
 
 function Shoe() {
     $(function () {
         $("#display").load("./shoe/shoe.html");
+        $('html, body').animate({
+            scrollTop: $("#nav-head").offset().top
+        }, 0);
     });
+
 }
 
 function orderDetail() {
     $(function () {
         $("#display").load("./shopping-cart/orderDetail.html");
+        $('html, body').animate({
+            scrollTop: $("#nav-head").offset().top
+        }, 0);
     });
 }
 
 function Home() {
     $(function () {
         $("#display").load("./home-page/home.html");
+        $('html, body').animate({
+            scrollTop: $("#nav-head").offset().top
+        }, 0);
     });
 }
 
 function signIn() {
     $(function () {
         $("#display").load("./login-register/signIn.html");
+        $('html, body').animate({
+            scrollTop: $("#nav-head").offset().top
+        }, 0);
     });
 }
 function signUp() {
     $(function () {
         $("#display").load("./login-register/signUp.html");
+        $('html, body').animate({
+            scrollTop: $("#nav-head").offset().top
+        }, 0);
     });
 }
 
 function Account() {
     $(function () {
         $("#display").load("./account/account.html");
+        $('html, body').animate({
+            scrollTop: $("#nav-head").offset().top
+        }, 0);
     });
 }
 
@@ -81,12 +117,30 @@ function SignUp() {
     });
 }
 
+function userMenu() {
+    $(function () {
+        $(".userMenu").toggleClass("hidden");
+    });
+}
+
 function SignIn() {
     $(function () {
         $(".signed").removeClass("hidden");
     });
     $(function () {
         $(".unsigned").addClass("hidden");
+    });
+    $(function () {
+        $("#display").load("./home-page/home.html");
+    });
+}
+
+function LogOut() {
+    $(function () {
+        $(".signed").addClass("hidden");
+    });
+    $(function () {
+        $(".unsigned").removeClass("hidden");
     });
     $(function () {
         $("#display").load("./home-page/home.html");
@@ -102,14 +156,13 @@ function cancelNoti() {
     });
 }
 
-//-----------------------
-
-$(document).ready(function () {
-    // Click event to load HTML content into the main div
-
-    $("#display").load("your-content.html", function () {
-        // Once content is loaded, scroll to the top
-        $("html, body").animate({ scrollTop: 0 }, "slow");
+function check() {
+    $(function () {
+        $(".checked").toggleClass("hidden");
     });
+    $(function () {
+        $(".uncheck").toggleClass("hidden");
+    });
+}
 
-});
+//-----------------------
